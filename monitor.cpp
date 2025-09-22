695c716de6fd1f386981ec3b1922d8b4133b5806
eri2kor <revi.hridya@in.bosch.com>
2025-09-22 17:59:16 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,38 +1,42 @@
 #include "./monitor.h"
-#include <assert.h>
+#include <iostream>
 #include <thread>
 #include <chrono>
-#include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+BreachType checkLimit(float value, const Limit& limit) {
+    if(value < limit.min) return BreachType::LOW;
+    if(value > limit.max) return BreachType::HIGH;
+    return BreachType::NORMAL;
+}
+
+std::string breachToString(BreachType breach) {
+    switch(breach) {
+        case BreachType::LOW: return "LOW";
+        case BreachType::HIGH: return "HIGH";
+        default: return "NORMAL";
     }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
+}
+
+// Simple blinking alert (I/O only)
+void blinkAlert() {
     for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
     }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+}
+
+int vitalsOk(const std::vector<Vital>& vitals) {
+    bool status = true;
+    for(const auto& v : vitals) {
+        BreachType breach = checkLimit(v.value, v.limit);
+        if(breach != BreachType::NORMAL) {
+            cout << v.name << " is " << breachToString(breach) << "!\n";
+            blinkAlert();
+            status = false;
+        }
     }
-    return 0;
-  }
-  return 1;
+    return status ? 1 : 0;
 }
