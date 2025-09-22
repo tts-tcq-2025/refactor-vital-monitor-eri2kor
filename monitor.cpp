ab4d0dadcf0cbd5667505e00d435325cc95b4ff3
eri2kor <revi.hridya@in.bosch.com>
2025-09-22 18:05:14 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -5,36 +5,39 @@
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
 BreachType checkLimit(float value, const Limit& limit) {
-    if(value < limit.min) return BreachType::LOW;
-    if(value > limit.max) return BreachType::HIGH;
+    if (value < limit.min) return BreachType::LOW;
+    if (value > limit.max) return BreachType::HIGH;
     return BreachType::NORMAL;
 }
 
 std::string breachToString(BreachType breach) {
-    switch(breach) {
+    switch (breach) {
         case BreachType::LOW: return "LOW";
         case BreachType::HIGH: return "HIGH";
         default: return "NORMAL";
     }
 }
 
-// Simple blinking alert (I/O only)
-void blinkAlert() {
-    for (int i = 0; i < 6; i++) {
+void blinkPattern(int cycles = 6, int delaySec = 1) {
+    for (int i = 0; i < cycles; i++) {
         cout << "\r* " << flush;
-        sleep_for(seconds(1));
+        sleep_for(seconds(delaySec));
         cout << "\r *" << flush;
-        sleep_for(seconds(1));
+        sleep_for(seconds(delaySec));
     }
+}
+
+void alert(const Vital& v, BreachType breach) {
+    cout << v.name << " is " << breachToString(breach) << "!\n";
+    blinkPattern();
 }
 
 int vitalsOk(const std::vector<Vital>& vitals) {
     bool status = true;
-    for(const auto& v : vitals) {
+    for (const auto& v : vitals) {
         BreachType breach = checkLimit(v.value, v.limit);
-        if(breach != BreachType::NORMAL) {
-            cout << v.name << " is " << breachToString(breach) << "!\n";
-            blinkAlert();
+        if (breach != BreachType::NORMAL) {
+            alert(v, breach);
             status = false;
         }
     }
