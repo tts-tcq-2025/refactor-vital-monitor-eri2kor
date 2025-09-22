b62342b966db8880700d7c1a8c0ef2f2659a0b2b
eri2kor <revi.hridya@in.bosch.com>
2025-09-23 01:13:47 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -10,13 +10,13 @@ struct VitalSign {
     double upperLimit;
 };
 
-enum class VitalStatus { ALERT_LOW, WARNING_LOW, NORMAL, WARNING_HIGH, ALERT_HIGH };
+enum class VitalStatus { NORMAL, WARNING_LOW, WARNING_HIGH, ALERT_LOW, ALERT_HIGH };
 
 // Pure functions
-VitalStatus checkVital(double value, double lower, double upper, double warningTolerance);
 double calculateWarningTolerance(double upper);
+VitalStatus checkVital(double value, double lower, double upper, double warningTolerance);
 
-// I/O functions
+// I/O function
 void displayStatus(const VitalSign& vital, VitalStatus status);
 
 #endif // MONITOR_H
