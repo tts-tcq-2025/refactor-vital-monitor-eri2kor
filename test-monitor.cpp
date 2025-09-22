29aae71673737d4e76151ca31c54a737aa96e8f0
eri2kor <revi.hridya@in.bosch.com>
2025-09-22 17:59:34 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,7 +1,65 @@
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+TEST(Monitor, AllVitalsNormal) {
+  std::vector<Vital> vitals = {
+    {"Temperature", 98.6, {95, 102}},
+    {"Pulse Rate", 72, {60, 100}},
+    {"Oxygen Saturation", 98, {90, 100}}
+  };
+  ASSERT_TRUE(vitalsOk(vitals));
+}
+
+TEST(Monitor, TemperatureTooHigh) {
+  std::vector<Vital> vitals = {
+    {"Temperature", 104, {95, 102}},
+    {"Pulse Rate", 72, {60, 100}},
+    {"Oxygen Saturation", 98, {90, 100}}
+  };
+  ASSERT_FALSE(vitalsOk(vitals));
+}
+
+TEST(Monitor, TemperatureTooLow) {
+  std::vector<Vital> vitals = {
+    {"Temperature", 94, {95, 102}},
+    {"Pulse Rate", 72, {60, 100}},
+    {"Oxygen Saturation", 98, {90, 100}}
+  };
+  ASSERT_FALSE(vitalsOk(vitals));
+}
+
+TEST(Monitor, PulseRateTooHigh) {
+  std::vector<Vital> vitals = {
+    {"Temperature", 98.6, {95, 102}},
+    {"Pulse Rate", 120, {60, 100}},
+    {"Oxygen Saturation", 98, {90, 100}}
+  };
+  ASSERT_FALSE(vitalsOk(vitals));
+}
+
+TEST(Monitor, PulseRateTooLow) {
+  std::vector<Vital> vitals = {
+    {"Temperature", 98.6, {95, 102}},
+    {"Pulse Rate", 50, {60, 100}},
+    {"Oxygen Saturation", 98, {90, 100}}
+  };
+  ASSERT_FALSE(vitalsOk(vitals));
+}
+
+TEST(Monitor, OxygenTooLow) {
+  std::vector<Vital> vitals = {
+    {"Temperature", 98.6, {95, 102}},
+    {"Pulse Rate", 72, {60, 100}},
+    {"Oxygen Saturation", 85, {90, 100}}
+  };
+  ASSERT_FALSE(vitalsOk(vitals));
+}
+
+TEST(Monitor, MultipleAbnormal) {
+  std::vector<Vital> vitals = {
+    {"Temperature", 104, {95, 102}},
+    {"Pulse Rate", 120, {60, 100}},
+    {"Oxygen Saturation", 85, {90, 100}}
+  };
+  ASSERT_FALSE(vitalsOk(vitals));
 }
