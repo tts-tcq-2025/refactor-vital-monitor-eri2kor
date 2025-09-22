ecc6e647657f909c03b3b673afd7936c80a73bcc
eri2kor <revi.hridya@in.bosch.com>
2025-09-22 17:58:55 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,3 +1,22 @@
 #pragma once
+#include <string>
 
-int vitalsOk(float temperature, float pulseRate, float spo2);
+enum class BreachType { LOW, HIGH, NORMAL };
+
+struct Limit {
+    float min;
+    float max;
+};
+
+struct Vital {
+    std::string name;
+    float value;
+    Limit limit;
+};
+
+// Pure functions
+BreachType checkLimit(float value, const Limit& limit);
+std::string breachToString(BreachType breach);
+
+// Monitoring function
+int vitalsOk(const std::vector<Vital>& vitals);
