b64b06bd9c501c9cb4620423843cb6bd55edcc59
eri2kor <revi.hridya@in.bosch.com>
2025-09-23 00:12:48 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -5,19 +5,19 @@
 enum class BreachType { LOW, HIGH, NORMAL, WARNING_LOW, WARNING_HIGH };
 
 struct Limit {
-    float min{};
-    float max{};
+    float min;
+    float max;
 };
 
 struct Vital {
     std::string name;
-    float value{};
-    Limit limit{};
+    float value;
+    Limit limit;
 };
 
 // Pure functions
 BreachType checkLimit(float value, const Limit& limit);
-BreachType checkLimitWithWarning(float value, const Limit& limit, float tolerance = 0.015f);
+BreachType checkLimitWithWarning(float value, const Limit& limit, float tolerance = 0.015);
 std::string breachToString(BreachType breach);
 
 // Monitoring function
